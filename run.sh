3fd9c9fb6ad21485e35dac14ba1f7f09b8152cfe
naoki suzuki <naokihirokitomoki@gmail.com>
2025-01-05 14:17:05 +0900

    add anticomp

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ if [ -z $MITM_USER ] || [ -z $MITM_PASS ]; then
   exit -1
 fi
 
-gosu mitmproxy mitmdump --listen-port 3126 --ssl-insecure -s /home/mitmproxy/files/flows.py --set stream_large_bodies=10m --ignore-hosts '(mzstatic|apple|icloud|mobilesuica|crashlytic|google-analytics|merpay|paypay|rakuten-bank|fate|colopl|rakuten-sec|line|kyash|plexure)' --set block_global=false --set flow_detail=1 --showhost --rawtcp --proxyauth ${MITM_USER}:${MITM_PASS}
+gosu mitmproxy mitmdump --listen-port 3126 --ssl-insecure -s /home/mitmproxy/files/flows.py --set stream_large_bodies=10m --ignore-hosts '(mzstatic|apple|icloud|mobilesuica|crashlytic|google-analytics|merpay|paypay|rakuten-bank|fate|colopl|rakuten-sec|line|kyash|plexure)' --set block_global=false --set flow_detail=1 --showhost --anticomp --rawtcp --proxyauth ${MITM_USER}:${MITM_PASS}
